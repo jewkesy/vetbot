133050ff11cb613fd6467f4dfefacea27a99a246
Daryl Jewkes <github@daryljewkes.com>
2017-11-28 15:33:59 +0000

    checkpoint

---
+++ ui/app/routes.js
@@ -0,0 +1,12 @@
+var path = require('path')
+var console = require('tracer').colorConsole();
+
+module.exports = function(app) {
+	// frontend routes =========================================================
+	// route to handle all angular requests
+
+	app.get('/', function(req, res) {
+		return res.render('home', {});
+	});
+
+};
